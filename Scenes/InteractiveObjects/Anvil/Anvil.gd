ef241f64cecec2465bc234c480e5d0f259182aef
indie-ink <indie.ink.st@gmail.com>
2025-05-29 21:41:27 +0200

    Added sparks when hitting Anvil

--- Scenes/InteractiveObjects/Anvil/Anvil.gd
+++ Scenes/InteractiveObjects/Anvil/Anvil.gd
@@ -3,8 +3,10 @@ class_name Anvil
 extends InteractiveObject
 
 const WAIT_AFTER_HIT_TIME := 0.7
+const PARTICLE_LIFETIME := 1
 
 @onready var pick_up_item: PickUpItem = $PickUpItem
+@onready var sparks_particles: CPUParticles2D = $SparksParticles
 
 @export var times_to_hit := 3
 
@@ -26,6 +28,8 @@ func request_player_hammer_hit() -> void:
 
 func handle_player_action_performed(action_type: Player.ActionTypes) -> void:
 	if action_type != Player.ActionTypes.HIT_ANVIL: return
+	
+	spawn_hit_effect()
 
 	_total_hits += 1
 	
@@ -35,3 +39,16 @@ func handle_player_action_performed(action_type: Player.ActionTypes) -> void:
 		_total_hits = 0
 		SignalHub.emit_anvil_passed()
 		pick_up_item.handle_picked_up()
+
+
+func spawn_hit_effect() -> void:
+	var new_particle: CPUParticles2D = sparks_particles.duplicate()
+
+	add_child(new_particle)
+	
+	new_particle.position = Vector2(0, -6)
+	new_particle.one_shot = true
+	new_particle.emitting = true
+	
+	await get_tree().create_timer(PARTICLE_LIFETIME).timeout
+	new_particle.call_deferred("queue_free")

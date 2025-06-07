a1a88a4735ef0fb3b9925afd605fe6809e7ebd22
indie-ink <indie.ink.st@gmail.com>
2025-06-07 16:23:26 +0200

    Send sold weapon to castle

--- Scenes/InteractiveObjects/WeaponRack/WeaponRack.gd
+++ Scenes/InteractiveObjects/WeaponRack/WeaponRack.gd
@@ -5,7 +5,10 @@ extends InteractiveObject
 const CREATED_WEAPON = preload("res://Scenes/InteractiveObjects/WeaponRack/CreatedWeapon/CreatedWeapon.tscn")
 const WAIT_FOR_THROW_TIME := 1.3
 const WAIT_BEFORE_SELL_TIME := 1.8
+const SEND_TO_SELL_POINT_TIME := 1
 const LAUNCH_IN_THE_AIR_FORCE := 400.0
+
+@export var sell_weapon_point: Marker2D = null
 
 @onready var pick_up_item: PickUpItem = $PickUpItem
 @onready var weapon_landing_marker: Marker2D = $WeaponLandingMarker
@@ -22,7 +25,7 @@ func handle_interaction() -> void:
 		SignalHub.emit_weapon_sold()
 		pick_up_item.handle_picked_up()
 		await get_tree().create_timer(WAIT_BEFORE_SELL_TIME).timeout
-		_current_weapon.call_deferred("queue_free")
+		send_weapon_to_marker()
 
 
 func spawn_weapon() -> void:
@@ -39,6 +42,19 @@ func animation_created_weapon(weapon: RigidBody2D) -> void:
 	middle.y -= LAUNCH_IN_THE_AIR_FORCE
 	weapon.apply_central_force(middle / 2)
 	throw_sound.play()
+
+
+func send_weapon_to_marker() -> void:
+	if !sell_weapon_point:
+		_current_weapon.call_deferred("queue_free")
+	else:
+		var tween: Tween = get_tree().create_tween()
+
+		tween.set_ease(Tween.EASE_OUT)
+
+		tween.parallel().tween_property(_current_weapon, "global_position", sell_weapon_point.global_position, SEND_TO_SELL_POINT_TIME)
+		tween.parallel().tween_property(_current_weapon, "rotation_degrees", 1080, SEND_TO_SELL_POINT_TIME)
+		tween.parallel().tween_property(_current_weapon, "scale", Vector2(0, 0), SEND_TO_SELL_POINT_TIME)
 
 
 func _on_weapon_landing_body_entered(body: Node2D) -> void:
